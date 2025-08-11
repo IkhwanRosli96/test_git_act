c38a6fa48dc73abc1e62023bb498f13cc237f16c
IkhwanRosli96 <ikhwanrosli0196@gmail.com>
2025-08-11 13:04:28 +0800

    test_git_act_2

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "This is automatically updated by the github actions"}
+    return {"status": "ok", "message": "This is automatically updated by the github actions again and again"}
 
 @app.get("/health")
 def health():
