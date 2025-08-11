fb6397b3e44ed67391724f86d3465e3366515ed6
IkhwanRosli96 <ikhwanrosli0196@gmail.com>
2025-08-11 14:56:19 +0800

    test_secret

--- app/main.py
+++ app/main.py
@@ -1,11 +1,18 @@
+mport os
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "This is automatically updated by the github actions again and again"}
+    return {"status": "ok", "message": "Hello from Docker CI sample!"}
 
 @app.get("/health")
 def health():
     return {"healthy": True}
+
+# ⚠️ TEST ONLY
+@app.get("/show-secret")
+def show_secret():
+    secret_value = os.getenv("MY_TEST_SECRET", "Secret not set")
+    return {"MY_TEST_SECRET": secret_value}
